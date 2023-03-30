e2a06013d53f0df5e168f480485319f5d92d1099
Samer Ghaly <samereaghaly@gmail.com>
2023-03-30 20:30:35 +0200

    remove dicesArray state to reduce re-render

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   const noOfDice = 10
   const [dicesData, setDices] = React.useState([]) // an array of dices data
-  const [dicesArray, setDicesArray] = React.useState([]) // an array of the dice components to be rendered
+  //const [dicesArray, setDicesArray] = React.useState([]) // an array of the dice components to be rendered
   const [selectedCounter, setSelectedCounter] = React.useState(0) // counter to show how many dices are selected
   const [finished, setFinished] = React.useState(false) // boolean to show game state
 
@@ -31,6 +31,8 @@ function App() {
   }
 
   function toggleSelect(id){
+    if(finished)
+      return
     setDices(prevDices => {
       var newDices = []
       var currentDice
@@ -109,22 +111,36 @@ function App() {
 
   React.useEffect(() => {
     gameFinished()
-    setDicesArray(dicesData.map((dice,index) => {
-      return(
-      <Dice 
-        key={index}
-        id={dice.id}
-        value={dice.value}
-        isSelected={dice.isSelected}
-        toggleSelect={toggleSelect}
-      />)
-      }))
+    // setDicesArray(dicesData.map((dice,index) => {
+    //   return(
+    //   <Dice 
+    //     key={index}
+    //     id={dice.id}
+    //     value={dice.value}
+    //     isSelected={dice.isSelected}
+    //     toggleSelect={toggleSelect}
+    //   />)
+    //   }))
   }, [dicesData])
+
+
+  const dicesArray = dicesData.map((dice,index) => {
+    return(
+    <Dice 
+      key={index}
+      id={dice.id}
+      value={dice.value}
+      isSelected={dice.isSelected}
+      toggleSelect={toggleSelect}
+    />)
+    })
+
+  console.log("render")
 
   return (
     <div className="app">
       <div className="main--container">
-        {finished && <Confetti recycle={false} numberOfPieces={600}/>}
+        {finished && <Confetti recycle={false} numberOfPieces={600} gravity={0.2}/>}
         <h1 className="game--title">Tenzies</h1>
         <p className="game--description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dices">
